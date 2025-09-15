b3443005fa543729d0ae1c80ba610691b6dd8c33
Iresh-Ghante <iresh.ghante@gmail.com>
2025-09-15 14:27:56 +0530

    update: getm api added

--- src/main/java/com/wehcamp/controller/HomeController.java
+++ src/main/java/com/wehcamp/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.wehcamp.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,10 @@ public class HomeController {
 	public ResponseEntity<String> getMassage(
 			@RequestParam(name = "massage", required = false, defaultValue = "User") String msg) {
 		return ResponseEntity.ok().body("Good morning " + msg);
+	}
+	
+	@GetMapping("/{msg}")
+	public ResponseEntity<String> getm(@PathVariable String msg){
+		return ResponseEntity.ok(msg);
 	}
 }
